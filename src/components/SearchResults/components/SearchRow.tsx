2c31874e52fe804cef0bd9ad8b43146d3792652d
Vitalii Shebela <vitalik.privat@gmail.com>
2019-03-23 11:08:26 +0100

    Added search row

--- src/components/SearchResults/components/SearchRow.tsx
+++ src/components/SearchResults/components/SearchRow.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { View, Text } from "react-native";
 
 type Props = {
   row: any
 }
 
 const SearchRow = ({ row }: Props) => (
-  <Text>{JSON.stringify(row)}</Text>
+  <View>
+    <Text>{row.id}</Text>
+    <Text>{row.name}</Text>
+    <Text>{row.owner.login}</Text>
+    <Text>{row['stargazers_count']}</Text>
+    <Text>{new Date(row.created_at).toLocaleDateString()}</Text>
+  </View>
 );
 
 export default SearchRow;
